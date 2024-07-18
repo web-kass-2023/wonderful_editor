4289c922e4c6e5d1c7fd04106b0223460fa0e5ea
柏平玲 <kashiwahira.git@gmail.com>
2024-07-18 22:48:09 +0900

    User model の作成 GitHub Actions の導入

--- db/migrate/20240717122924_devise_token_auth_create_users.rb
+++ db/migrate/20240717122924_devise_token_auth_create_users.rb
@@ -29,7 +29,6 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.1]
 
       ## User Info
       t.string :name
-      t.string :nickname
       t.string :image
       t.string :email
 
