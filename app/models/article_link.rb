ed86bc6850d6f748c5838b5a3dd7dfea7ec54b5e
柏平玲 <kashiwahira.git@gmail.com>
2024-07-19 13:10:48 +0900

    ER図に書いた各種テーブルとmodelの作成【model の作成】

---
+++ app/models/article_link.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: article_links
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  article_id :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_article_links_on_article_id  (article_id)
+#  index_article_links_on_user_id     (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (article_id => articles.id)
+#  fk_rails_...  (user_id => users.id)
+#
+class ArticleLink < ApplicationRecord
+  belongs_to :user
+  belongs_to :article
+end
---
+++ spec/factories/article_links.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: article_links
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  article_id :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_article_links_on_article_id  (article_id)
+#  index_article_links_on_user_id     (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (article_id => articles.id)
+#  fk_rails_...  (user_id => users.id)
+#
+FactoryBot.define do
+  factory :article_link do
+    user { nil }
+    article { nil }
+  end
+end
